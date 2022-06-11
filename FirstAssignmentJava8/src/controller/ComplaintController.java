665af9d2af686727ca3802ac6962edfd54badfca
Krishna Yadav <krishs09@yahoo.in>
2022-06-11 12:25:06 +0530

    Update ComplaintController.java

--- FirstAssignmentJava8/src/controller/ComplaintController.java
+++ FirstAssignmentJava8/src/controller/ComplaintController.java
@@ -18,35 +18,51 @@ public class ComplaintController {
 		IComplaintService srvc = new ComplaintServiceImpl();
 		
 		
-		System.out.println("<------------Get Complaints By ID------------------>");
-		Scanner input = new Scanner(System.in);
-		System.out.println("Please Enter complaint ID");
-		long complaintID = input.nextLong();  
+		Scanner menu = new Scanner(System.in);
+		System.out.println("Please select Your choice");
+		System.out.println("1) Get complaints by ID"+"\n"+"2) Get complaints by Year"+"\n"+"3) Get complaints by bank name"
+				+"\n"+"4) List time taken by each bank to resolve complaints"+"\n"+"0) Exit");
+		int menuSelected = menu.nextInt();
 		
-		srvc.getComplaintsByComplaintID(complaintID);
-		
-	
-		
-		System.out.println("<------------Get Complaints By Year------------------>");
-		Scanner input2 = new Scanner(System.in);
-		System.out.println("Please Enter Year");
-		long year = input2.nextLong();  
-		
-		srvc.getComplaintsByYear(year);
-		
-		
-		System.out.println("<---------Get Complaints By Bank Name--------------------->");
-		Scanner input3 = new Scanner(System.in);
-		System.out.println("Please Enter Bank name");
-		String bankName = input3.next();
-		
-		srvc.getComplaintsByBankName(bankName);
-		
-		
-		System.out.println("<---------List Tim taken by bank to close request--------------------->");
-		srvc.listTimeTakenByBankToCloseComplaint();
-		
-		
+		switch(menuSelected){
+			
+			case 1:
+				System.out.println("<------------Get Complaints By ID------------------>");
+				Scanner input = new Scanner(System.in);
+				System.out.println("Please Enter complaint ID");
+				long complaintID = input.nextLong();  
+				
+				srvc.getComplaintsByComplaintID(complaintID);
+				break;
+			case 2:
+				System.out.println("<------------Get Complaints By Year------------------>");
+				Scanner input2 = new Scanner(System.in);
+				System.out.println("Please Enter Year");
+				long year = input2.nextLong();  
+				
+				srvc.getComplaintsByYear(year);
+				break;
+			case 3:
+				System.out.println("<---------Get Complaints By Bank Name--------------------->");
+				Scanner input3 = new Scanner(System.in);
+				System.out.println("Please Enter Bank name");
+				String bankName = input3.next();
+				
+				srvc.getComplaintsByBankName(bankName);
+				
+				break;
+			case 4:
+
+				
+				System.out.println("<---------List Tim taken by bank to close request--------------------->");
+				srvc.listTimeTakenByBankToCloseComplaint();
+				break;
+			case 0:
+				System.out.println("Thank you, good bye!");
+				System.exit(0);
+				break;
+				
+		}
 		
 	}
 
